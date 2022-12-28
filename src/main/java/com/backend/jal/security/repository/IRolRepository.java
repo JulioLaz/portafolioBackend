0062a3929e5a16937578d724e1a2e8b8ef62be93
JulioLaz <julioalazarte@gmail.com>
2022-12-28 19:02:13 -0300

    proyecto portafolio concluido

---
+++ src/main/java/com/backend/jal/security/repository/IRolRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.backend.jal.security.repository;
+
+import com.backend.jal.security.entity.Rol;
+import com.backend.jal.security.enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IRolRepository extends JpaRepository<Rol, Integer>{
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
+}
